7df7c87a965d676390dc6c5256a84971ed22d850
gezever <gezever@skynet.be>
2024-03-07 15:29:23 +0100

    files en dir in package

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -5,14 +5,15 @@ import rdf from "@zazuko/env-node";
 import convert from "xml-js";
 import jp from "jsonpath";
 
+const this_config = yaml.load(fs.readFileSync(process.cwd() + '/../node_modules/maven-metadata-generator-npm/src/config/config.yml', 'utf8'));
 
 const config = yaml.load(fs.readFileSync('./source/config.yml', 'utf8'));
 
 const context = Object.assign({},JSON.parse(readFileSync(config.source.path + config.source.context)), config.prefixes)
 
-const shapes_dcat = await rdf.dataset().import(rdf.fromFile(config.ap.path + 'dcat-' + config.ap.type + '/' + 'dcat-' + config.ap.type + config.ap.turtle))
+const shapes_dcat = await rdf.dataset().import(rdf.fromFile(process.cwd() + this_config.ap.path))
 
-const pom = JSON.parse(convert.xml2json(readFileSync(config.pom.path, 'utf8'), {compact: true, spaces: 4}));
+const pom = JSON.parse(convert.xml2json(readFileSync(this_config.pom.path, 'utf8'), {compact: true, spaces: 4}));
 
 const groupId = jp.query(pom, '$.project.groupId._text').toString();
 
@@ -20,7 +21,7 @@ const artifactId = jp.query(pom, '$.project.artifactId._text').toString();
 
 const next_release_version = jp.query(pom, '$.project.version._text').toString().split('-')[0];
 
-const dcat_rules = config.dcat.rules ;
+const dcat_rules = this_config.dcat.rules ;
 
 const dcat_catalog_path = config.dcat.path_catalog + artifactId + '/'
 
